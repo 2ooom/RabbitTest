17e6337d23648bf463f18a3190f787290995656b
Dmitry Parfenchik <dmitry.parfenchik@gmail.com>
2015-05-27 17:31:44 +0200

    Exchange example implementation

---
+++ LogPublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace LogPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", "fanout");
+                    while (true)
+                    {
+                        Console.WriteLine("Press a key");
+                        var msg = Console.ReadLine();
+                        var body = Encoding.UTF8.GetBytes(msg);
+                        channel.BasicPublish("logs", "", null, body);
+                        Console.WriteLine(" ** [{0}s {1}ms] Send message: {2}", DateTime.Now.Second, DateTime.Now.Millisecond, msg);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LogReceiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace LogReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queueName, "logs", "");
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(queueName, true, consumer);
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    while (true)
+                    {
+                        var packet = consumer.Queue.Dequeue();
+                        var body = packet.Body;
+                        var msg = Encoding.UTF8.GetString(body);
+                        var sleep = msg.Split('.').Length;
+                        Console.WriteLine(" [x] Working on message: [{0}] at {1}s {2}ms", msg, DateTime.Now.Second, DateTime.Now.Millisecond);
+                        Thread.Sleep(sleep * 1000);
+                        Console.WriteLine(" [x] Message processed");
+                    }
+                }
+            }
+        }
+    }
+}
