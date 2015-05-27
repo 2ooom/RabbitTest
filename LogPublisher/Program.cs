b9694f8268faf920f86419820c1e2ad3ede2e38b
Dmitry Parfenchik <dmitry.parfenchik@gmail.com>
2015-05-27 18:10:50 +0200

    Direct message exchange implementation

--- LogPublisher/Program.cs
+++ LogPublisher/Program.cs
@@ -6,6 +6,7 @@ namespace LogPublisher
 {
     class Program
     {
+        private static string exchangeName = "direct_logs";
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory { HostName = "localhost" };
@@ -13,14 +14,22 @@ namespace LogPublisher
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.ExchangeDeclare("logs", "fanout");
+                    channel.ExchangeDeclare(exchangeName, "direct");
                     while (true)
                     {
-                        Console.WriteLine("Press a key");
-                        var msg = Console.ReadLine();
+                        Console.WriteLine("input message");
+                        var line = Console.ReadLine();
+                        if (string.IsNullOrEmpty(line) || line.IndexOf(' ') < 0)
+                        {
+                            Console.WriteLine("Format is: {Seveirity} {Message}");
+                            continue;
+                        }
+                        var delimImndex = line.IndexOf(' ');
+                        var severity = line.Substring(0, delimImndex);
+                        var msg = line.Substring(delimImndex + 1);
                         var body = Encoding.UTF8.GetBytes(msg);
-                        channel.BasicPublish("logs", "", null, body);
-                        Console.WriteLine(" ** [{0}s {1}ms] Send message: {2}", DateTime.Now.Second, DateTime.Now.Millisecond, msg);
+                        channel.BasicPublish(exchangeName, severity, null, body);
+                        Console.WriteLine(" ** [{0}s {1}ms] [{3}] Send message: {2}", DateTime.Now.Second, DateTime.Now.Millisecond, msg, severity);
                     }
                 }
             }
--- LogReceiver/Program.cs
+++ LogReceiver/Program.cs
@@ -7,17 +7,28 @@ namespace LogReceiver
 {
     class Program
     {
+        private static string exchangeName = "direct_logs";
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Specify at severity to subscribe for");
+                return;
+            }
             var factory = new ConnectionFactory { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.ExchangeDeclare("logs", "fanout");
+                    channel.ExchangeDeclare(exchangeName, "direct");
 
                     var queueName = channel.QueueDeclare().QueueName;
-                    channel.QueueBind(queueName, "logs", "");
+                    foreach (var severity in args)
+                    {
+                        channel.QueueBind(queueName, exchangeName, severity);
+                        Console.WriteLine(" [i] Subscribing for log messages of severity [{0}]", severity);
+                    }
+                    
                     var consumer = new QueueingBasicConsumer(channel);
                     channel.BasicConsume(queueName, true, consumer);
 
@@ -29,7 +40,7 @@ namespace LogReceiver
                         var body = packet.Body;
                         var msg = Encoding.UTF8.GetString(body);
                         var sleep = msg.Split('.').Length;
-                        Console.WriteLine(" [x] Working on message: [{0}] at {1}s {2}ms", msg, DateTime.Now.Second, DateTime.Now.Millisecond);
+                        Console.WriteLine(" [x] [{3}] Working on message: [{0}] at {1}s {2}ms", msg, DateTime.Now.Second, DateTime.Now.Millisecond, packet.RoutingKey);
                         Thread.Sleep(sleep * 1000);
                         Console.WriteLine(" [x] Message processed");
                     }
