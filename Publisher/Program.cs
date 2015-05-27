862d56d130a1e6da7e19404ed8d9d638951ae7c6
Dmitry Parfenchik <dmitry.parfenchik@gmail.com>
2015-05-27 15:31:41 +0200

    Initial commit with basic Publisher / Consumer scenarion

---
+++ Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    while (true)
+                    {
+                        Console.WriteLine("Press a key");
+                        var msg = Console.ReadLine();
+                        var body = Encoding.UTF8.GetBytes(msg);
+                        channel.BasicPublish("", "hello", null, body);
+                        Console.WriteLine(" ** [{0}s {1}ms] Send message: {2}", DateTime.Now.Second, DateTime.Now.Millisecond, msg);
+                    }
+                }
+            }
+        }
+    }
+}
